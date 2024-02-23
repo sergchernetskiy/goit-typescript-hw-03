25ddf9c4a95784469c25203935f4d3ce9226dde3
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2024-02-23 20:49:03 +0200

    classes

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    console.log(this.signature);
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("The door is open");
+    } else {
+      console.log("The door is close");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open");
+    } else {
+      console.log("The door is close");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +57,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
